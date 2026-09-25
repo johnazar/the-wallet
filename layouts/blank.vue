c34024bd5cfc3a87894d31280c13dfe8b4c1d4de
John MBP <john@users.noreplay.github.com>
2026-09-25 13:52:25 +0400

    remove old code

--- layouts/blank.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="min-h-screen flex items-center justify-center bg-gray-50 p-4">
-    <slot />
-  </div>
-</template>
-
-<script setup lang="ts">
-// blank layout: no header or footer, used for auth/login pages
-</script>
